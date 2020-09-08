f7d7a6fc4691f38cb090c3c996ae7649250b745e
Arefin Islam <arefin.ruet.eee16@gmail.com>
2020-09-08 22:06:29 +0600

    Create Loop over Numpy array.py

---
+++ Intermediate_python/Loop over Numpy array.py
@@ -0,0 +1,10 @@
+# Import numpy as np
+import numpy as np
+
+# For loop over np_height
+for item in np_height:
+    print(str(item)+" inches")
+
+# For loop over np_baseball
+for a in np.nditer(np_baseball):
+    print(a)
